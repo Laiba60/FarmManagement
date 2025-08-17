75cbf6500bf54ec6971e4792f4968a739bdcd0fa
Laiba60 <laibasaeed026@gmail.com>
2025-08-17 07:04:10 -0700

    initail commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  build: {
-    outDir: 'dist', // ensure Vercel knows output folder
-  },
+  plugins: [react()],
+  base: './',   // ✅ ensures relative paths for assets
 })
