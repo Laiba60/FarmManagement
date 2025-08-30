8f900d85f79b9a029096361302c5407e467e03f0
Laiba60 <laibasaeed026@gmail.com>
2025-08-30 06:29:42 -0700

    improve design

--- server/server.js
+++ server/server.js
@@ -1,39 +1,37 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors'); // ✅ Import CORS
+const cors = require('cors'); 
 const connectDB = require('./database');
-const setupSwagger = require('./swagger');  // Import swagger setup
+const setupSwagger = require('./swagger');  
 
 const app = express();
 
-// ===== CORS Middleware =====
-// Allow requests from your frontend (http://localhost:5173)
-// You can also use app.use(cors()) to allow all origins during development
+
 app.use(cors({
     origin: (origin, callback) => {
-        // allow requests with no origin (Postman, curl)
+       
         if (!origin) return callback(null, true);
 
-        // allow any localhost origin
+        
         if (origin.startsWith('http://localhost')) {
             return callback(null, true);
         }
 
-        // block other origins
+       
         callback(new Error('Not allowed by CORS'));
     },
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
-// Middleware for JSON
+
 app.use(express.json());
 
-// Connect to MongoDB
+
 connectDB();
 
-// Swagger docs route
-setupSwagger(app);  // Swagger API docs available at /api-docs
-// Dummy data
+
+setupSwagger(app);  
+
 const engineers = [
   { name: "Ali", expertise: "Precision Spraying", status: "Available" },
   { name: "Sara", expertise: "GPS Navigation", status: "Busy" },
@@ -43,7 +41,7 @@ const engineers = [
 let batteryStatus = { value: 75 }; 
 let chemicalLevel = { value: 60 };
 
-// Routes
+
 app.get("/engineers", (req, res) => {
   res.json(engineers);
 });
@@ -55,14 +53,14 @@ app.get("/battery", (req, res) => {
 app.get("/chemical", (req, res) => {
   res.json(chemicalLevel);
 });
-// Routes
+
 const authRoutes = require('./routes/auth');
 app.use('/auth', authRoutes);
 
 const dashboardRoutes = require('./routes/dashboard');
 app.use('/dashboard', dashboardRoutes);
 
-// Chatbot Routes
+
 const chatbotRoutes = require('./routes/chatbot');
 app.use('/chatbot', chatbotRoutes);
 
@@ -76,7 +74,7 @@ app.use('/robots', robotsRoutes);
 const missionsRoutes = require("./routes/missions");
 app.use("/missions", missionsRoutes);
 
-// Test Routes
+
 app.get('/', (req, res) => {
     res.send('🚀 Farm Management API Running');
 });
@@ -89,6 +87,6 @@ app.get('/faqs', (req, res) => {
     res.json(faqs);
 });
 
-// Start Server
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
